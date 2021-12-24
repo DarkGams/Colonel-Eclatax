8234e242384eb14647f3bfdf9f3c098aa688ec30
Gam's <82679287+DarkGams@users.noreply.github.com>
2021-12-24 01:33:19 +0100

    Create Colonel_Eclatax.py

---
+++ Colonel_Eclatax.py
@@ -0,0 +1,124 @@
+import time
+import os 
+import random
+from pystyle import Anime, Colors, Colorate, Center, Box
+
+banner = """----------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+-------------------------------------------.-------------.------------------------------------------
+----------------------------------------.---:/oyyhddhhys+:------------------------------------------
+-----------------------------------------/sdmNNNmmddhhhmmmy+----------------------------------------
+------------------------------------.--/yddhhyyyhhdmmmdhyhmNd+--------------------------------------
+-------------------------------------:ydhyssssssyyhyyhmNmhshmNs-------------------------------------
+------------------------------------omdyssssssshddNdssyhmNhsydNy------------------------------------
+-----------------------------------sNhssssysssds-/NmssssydNdysdNy-----------------------------------
+----------------------------------sNhsssydhdyds../NmsyyssshNmysdNs----------------------------------
+---------------------------------oNhsssym+-sNs--:sNmhmmyssshNdssmNs---------------------------------
+---------------------------------mmsssshd-../:/ds/:--hdsssssdNdsyNN+--------------------------------
+--------------------------------/NhsssyyNs.+hoohmys:odsssssssmNhshNm:.------------------------------
+--------------------------------/NysyNyss++Nh..-md-.syymmdsssyNNysdNd-------------------------------
+---------------------------------mhsyN+...+Nh..:N/..../mNNdysshNmysmNy------------------------------
+--------------------------------.yNdmmms::-hNh+dN/:--odhydNNhsshNmsymNo.----------------------------
+---------------------------------/NNNdmdhyssssssyyhdmNdhyyhNNdysdNdshNNo----------------------------
+---------------------------------/mds///+++++++++/::/+yhddhhNNmysmNdsymNy:--------------------------
+---------------------------------hNdhdmmdhhmdhhNNNmdhs+::ohmNNNNdydNmyydmmmmdhysso+/:--.------------
+-------------------------------+hNNNNNdyyhhsshmNNNNNNNNmy+:hNhmNNmyhmNmhyhdmmddddmmmmy-.------------
+-------------------------------+yhmNNmmmNmddNNNNNNNNNNNNNNmNs-:mNNNhyhdmmdhyyyyyyyyyNNo-------------
+----------------------------------:dmsooyyyysssssssyyyyhhddmdyomhodNmhyyyhdmmmmddmmNNh:.------------
+---------------------------------.+mmNmmdyo:-------+++-----::+yN:.-dNNNmdhyyyyyhdNNd+---------------
+----------------------------------ho:hho:---------:hdy---------hhyymmyhhdNNmmmmmds/-----------------
+---------------------------------:d-/m----------------::------/mNNNNNdyydNyyso+:---.----------------
+----------------------------y:.--yo-so------------sddso:-----/mNNNNNNNmdNy.-------------------------
+--------------------------.ym:--+d-/h-------------:dd-------/mNNmyo/+yNNN/--------------------------
+---------------------------dNo..ys-hdsssydmms/----yN/------+NNNo--+/--dNy---------------------------
+---------------------------oNm//mNmNNNNmNNNNNNmysdNh::::--+mNm/-/ydh-+Nm:---------------------------
+----------------------------smNNNNNNNN+./yhNNNNNNmh/::::-oNNN/-od:::-yNs.---------------------------
+-----------------------------:dNhso+mN:  ``hNyso+:::::::sNNNy--oN/--sNN/----------------------------
+--------------------------.--oNs---/yhhys/:hy--------:/hNNNd/--+y-:hNNd-----------------------------
+----------------------------+Ns-------+ooyhy:--------+mNNNN/----:omNNNs.----------------------------
+---------------------------:my--------oo-------------hddhhs---+hmNNNNm:-----------------------------
+---------------------------hd---::----------------------------:oNNNNm+.-----------------------------
+--------------------------/Ns--/d/------------------------------hNdo:.------------------------------
+------------------------.-:Nh--hN/------/y:--------------------omo----------------------------------
+---------------------------sNo-yNNdyssydd+--------------------sd/-----------------------------------
+----------------------------omd//syhhyo:--------------------:dd:------------------------------------
+------------------------------yms--------------------------omm:-------------------------------------
+-----------------------------.-/dd+--------------------/osdNNo--------------------------------------
+------------------------------.--odh/------------:+oshhhs/-yN/--------------------------------------
+----------------------------------:yms++++++oosyhhyo:.``  `yNm/-------------------------------------
+-----------------------------------oNNNNNNmys+:-.``      :hdhNm/------------------------------------
+---------------------------------/hNNNNNNN-`           -shyssyNm/--::-------------------------------
+----------------------------:::+hmNNNNNNNN/          .odhyyhhdNNmhhhhhs/:---------------------------
+--------------------::/+oshddmmmhhNNNNNmyNm.       .+hhsdmNNNddhhhhyyyhdhyo:----.-------------------
+----------:://ossyyhhhddhhhhdNmyshNNNNy- +Nh`    .oddyyyNdhhdmmmmmdhdmdyyhhdhs/---------------------
+------:yhhddddhhhyyyyyhhddmmNdshmmNNd:`  `yN+  `/ddhddNNmmNmhyhhmNNmdhhyyyyyhdmhs/------------------
+------+NmyyyyyhhddmmmmmmdmNmyyhNNNNNd-    .mm/oddyddydNhsmNmNmdhyhmNNNdddhyo+/+yNmdho--.------------
+-------yNNmmmNNNNNNhysssymdyhNNNNNNNNm/    +NNdysyNmmmyydmysyhdmmmmhmNNNNdshhhhhyyyhmms---.-.-------
+--------sNmysmNNNNdsssshmhsdNNNNNNNNNNN- `omdyshhyydhsymdsssssssyhmNNmhdmNNdhhhdddmmmNNmo--.--------"""
+
+print(Center.Center(Colorate.Horizontal(Colors.green_to_blue, (banner), 1)))
+
+time.sleep(3)
+
+os.system("cls")
+
+banner_2 = (Center.XCenter(Box.DoubleCube("""  ______   ______    __        ______   .__   __.  _______  __          _______   ______  __          ___   .___________.    ___      ___   ___ 
+ /      | /  __  \  |  |      /  __  \  |  \ |  | |   ____||  |        |   ____| /      ||  |        /   \  |           |   /   \     \  \ /  / 
+|  ,----'|  |  |  | |  |     |  |  |  | |   \|  | |  |__   |  |        |  |__   |  ,----'|  |       /  ^  \ `---|  |----`  /  ^  \     \  V  /  
+|  |     |  |  |  | |  |     |  |  |  | |  . `  | |   __|  |  |        |   __|  |  |     |  |      /  /_\  \    |  |      /  /_\  \     >   <   
+|  `----.|  `--'  | |  `----.|  `--'  | |  |\   | |  |____ |  `----.   |  |____ |  `----.|  `----./  _____  \   |  |     /  _____  \   /  .  \  
+ \______| \______/  |_______| \______/  |__| \__| |_______||_______|   |_______| \______||_______/__/     \__\  |__|    /__/     \__\ /__/ \__\ """)))
+
+btn = (Center.Center(Box.DoubleCube("Appuyez sur ENTRER pour commencer")))
+Anime.Fade((banner_2 + btn), color=Colors.green_to_blue, mode=Colorate.Diagonal, enter=True)
+print(banner_2 + btn)
+
+os.system("cls")
+
+sentence_1 = (Colors.blue + "\nLe colonel Eclatax vous souhaite la bienvenue a bord !\nVous etes ici pour fummer un bon bedo et voir a quel point vous resistez a la fumette ?\n\nLes regles sont simples :\n> Vous fummez autant de joints qu'il y a de personnes\n> Quand je dis <<taff>> vous ferez une taff\n> Quand je dirais <<pomme-de-terre>> vous ferez un turbo de fou fou batard dans votre main !\n> Ne recrachez pas tant que je n'ai pas dit <<Recrachez>>\n> Si vous ne tenez pas jusqu'au bout vous etes une merde\n\n")
+
+for char in sentence_1:
+	time.sleep(0.1)
+	print(char, end='', flush=True)
+
+ENTER = input(Box.DoubleCube("Tapez ENTRER pour continuer"))
+
+def level_1():
+	taff = """_____         ________________
+__  /_______ ____  __/___  __/
+_  __/_  __ `/__  /_  __  /_  
+/ /_  / /_/ / _  __/  _  __/  
+\__/  \__,_/  /_/     /_/     
+                              """
+
+	turbo = """________ ______ _______ ___ _______ ___ _____      
+___  __ \_  __ \__  __ `__ \__  __ `__ \_  _ \     
+__  /_/ // /_/ /_  / / / / /_  / / / / //  __/     
+_  .___/ \____/_/_/_/_/ /_/ /_/ /_/ /_/ \___/      
+/_/        ______  /_____                          
+           _  __  / _  _ \                         
+           / /_/ /  /  __/                         
+_____      \__,_/   \___/                          
+__  /______ _____________________                  
+_  __/_  _ \__  ___/__  ___/_  _ \                 
+/ /_  /  __/_  /    _  /    /  __/                 
+\__/  \___/ /_/     /_/     \___/        """
+
+	spitout = """                                            ______              
+_____________ _____________________ ___________  /_ _____ ______
+__  ___/_  _ \_  ___/__  ___/_  __ `/_  ___/__  __ \_  _ \___  /
+_  /    /  __// /__  _  /    / /_/ / / /__  _  / / //  __/__  /_
+/_/     \___/ \___/  /_/     \__,_/  \___/  /_/ /_/ \___/ _____/
+                                                                """
+
+	var = taff , turbo , spitout
+
+	for i in range(20):
+		game = random.choice(var)
+		print(Center.Center(Box.DoubleCube(game)))
+		time.sleep(4)
+		os.system("cls")
+
+if ENTER == '':
+	os.system("cls")
+	level_1()
